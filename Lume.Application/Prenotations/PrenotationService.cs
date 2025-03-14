5e867502c8d96e75dc2b2452760484c0229a267b
Rohozin Maksym <rohozin.max@gmail.com>
2025-03-14 10:14:16 +0100

    General documentation

--- Lume.Application/Prenotations/PrenotationService.cs
+++ Lume.Application/Prenotations/PrenotationService.cs
@@ -2,6 +2,10 @@ using Lume.Domain.Repositories;
 
 namespace Lume.Application.Prenotations;
 
+/// <summary>
+/// Service for managing prenotations
+/// </summary>
+/// <param name="prenotationRepository">Operations</param>
 internal class PrenotationService(IPrenotationRepository prenotationRepository) : IPrenotationService
 {
     
